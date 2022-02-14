ded4b548bb73e9850cf1fc37771aa4869ef18d78
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-02-14 17:45:46 +0100

    finished base exercise

--- src/Pages/QuotesPage.tsx
+++ src/Pages/QuotesPage.tsx
@@ -10,9 +10,17 @@ type Quote = {
     quote: string
 }
 
+type newQuote = {
+    firstName: string,
+    lastName: string,
+    age: number,
+    avatar: string,
+    quote: string
+}
+
 export default function QuotesPage() {
 
-    const [quotes, setQuotes] = useState<Quote[]>([])
+    const [quotes, setQuotes] = useState<(Quote)[]>([])
 
     const [firstName, setFirstName] = useState<string>('')
     const [lastName, setLastName] = useState<string>('')
@@ -20,7 +28,7 @@ export default function QuotesPage() {
     const [avatar, setAvatar] = useState<string>('')
     const [age, setAge] = useState<number>(0)
 
-    const [formQuote, setFormQuote] = useState<Quote | null>(null)
+    const [formQuote, setFormQuote] = useState<Quote | newQuote |  null>(null)
     const [quotesSearch, setQuotesSearch] = useState<Quote[]>([])
     
     let term: string = 'life'
@@ -52,14 +60,30 @@ export default function QuotesPage() {
     
     function handleFormSubmit(e:any) {
 
-        const newQuoteObject:Quote = {
-            id: 13,
+        e.preventDefault()
+        
+        const newQuoteObject:newQuote = {
             firstName: firstName,
             lastName: lastName,
-            age: age,
+            age: Number(age),
             quote: quote,
             avatar: avatar
         }
+
+        fetch(`http://localhost:8000/quotes`, {
+        
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newQuoteObject)
+        })
+            .then(responseItem => responseItem.json())
+            
+            .then(newQuote => {
+                const updatedQuotes:Quote[] = [...quotes, newQuote]
+                setQuotes(updatedQuotes)
+            })
 
         setFormQuote(newQuoteObject)
         
@@ -109,36 +133,44 @@ export default function QuotesPage() {
 
             </ul>
 
-            <form className="form-quotes" onSubmit={function (e) {
-                handleFormSubmit(e)
-            }}>
+            <div className="form-wrapper">
 
-                <input name="first-name" defaultValue={firstName} type="text" placeholder="Enter first name: " 
-                onChange={(e) => {
-                    handleFirstNameChange(e)
-                }}></input>
+                <form className="form-quotes" onSubmit={function (e) {
+                    handleFormSubmit(e)
+                }}>
 
-                <input name="last-name" defaultValue={lastName} type="text" placeholder="Enter last name: " 
-                onChange={(e) => {
-                    handleLastNameChange(e)
-                }}></input>
+                    <input name="first-name" defaultValue={firstName} type="text" placeholder="Enter first name: " 
+                    onChange={(e) => {
+                        handleFirstNameChange(e)
+                    }}></input>
 
-                <input name="quote" defaultValue={quote} type="text" placeholder="Enter quote: " 
-                onChange={(e) => {
-                    handleQuoteChange(e)
-                }}></input>
+                    <input name="last-name" defaultValue={lastName} type="text" placeholder="Enter last name: " 
+                    onChange={(e) => {
+                        handleLastNameChange(e)
+                    }}></input>
 
-                <input name="avatar" defaultValue={avatar} type="text" placeholder="Enter avatar: " 
-                onChange={(e) => {
-                    handleAvatarChange(e)
-                }}></input>
+                    <input name="quote" defaultValue={quote} type="text" placeholder="Enter quote: " 
+                    onChange={(e) => {
+                        handleQuoteChange(e)
+                    }}></input>
 
-                <input name="age" defaultValue={age} type="text" placeholder="Enter age: " 
-                onChange={(e) => {
-                    handleAgeChange(e)
-                }}></input>
+                    <input name="avatar" defaultValue={avatar} type="text" placeholder="Enter avatar: " 
+                    onChange={(e) => {
+                        handleAvatarChange(e)
+                    }}></input>
 
-            </form>
+                    <input name="age" defaultValue={age} type="text" placeholder="Enter age: " 
+                    onChange={(e) => {
+                        handleAgeChange(e)
+                    }}></input>
+
+                    <button>
+                        Submit
+                    </button>
+
+                </form>
+
+            </div>
             
         </>
 
